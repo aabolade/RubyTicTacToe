dfd7639416b1e6cdcbcac4231f04a98a5b916ce2
Leke Abolade <leke_abolade92@hotmail.co.uk>
2017-07-06 20:10:33 +0100

    adding tests to check for a winner

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -6,6 +6,7 @@ describe Game do
   let(:board_string) {"0 | 1 | 2 \n===+===+===\n 3 | 4 | 5 \n===+===+===\n 6 | 7 | 8 \n"}
   let(:tie_board) {["X", "O", "X", "X","O","X","O","X","O"]}
   let(:no_tie_board) {["X","1","O","3","X","O","X","O","8"]}
+  let(:winner) {["O","O","O","X","X","5","6","7","8"]}
 
   describe "displaying the board" do
     it "returns the state of the current board as a string" do
@@ -17,6 +18,16 @@ describe Game do
   describe "ending the game" do
     it "displays a message to end the game" do
       expect(game.display_message_for_end_of_game).to eq "Game over"
+    end
+  end
+
+  describe "checking for when the game is over" do
+    it "returns true if either player has won the game" do
+      expect(game.game_is_over(noughts_winner)).to eq true
+    end
+
+    it "returns false if there is no winner" do
+      expect(game.game_is_over(board_string)).to eq false
     end
   end
 
